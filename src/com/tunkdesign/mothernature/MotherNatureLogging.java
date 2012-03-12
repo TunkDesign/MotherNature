46d8f0239a8b2c74d45ee1258bfdb5532caf63c1
Karl Hakansson <webmaster@tunkdesign.com>
2012-03-12 20:16:27 +0100

    File added

---
+++ src/com/tunkdesign/mothernature/MotherNatureLogging.java
@@ -0,0 +1,40 @@
+package com.TunkDesign.MotherNature;
+
+//Java import
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//Referenced classes of package com.TunkDesign.MotherNature:
+//          MotherNatureSettings
+
+public class MotherNatureLogging
+{
+
+  public MotherNatureLogging()
+  {
+  }
+
+  public void info(String s)
+  {
+      logger.log(Level.INFO, (new StringBuilder("[MotherNature] ")).append(s).toString());
+  }
+
+  public void debug(String s)
+  {
+      if(MotherNatureSettings.debugMode)
+          logger.log(Level.INFO, (new StringBuilder("[MotherNature DEBUG] ")).append(s).toString());
+  }
+
+  public void severe(String s)
+  {
+      logger.log(Level.SEVERE, (new StringBuilder("[MotherNature] ")).append(s).toString());
+  }
+
+  public void warning(String s)
+  {
+      logger.log(Level.WARNING, (new StringBuilder("[MotherNature] ")).append(s).toString());
+  }
+
+  public static final Logger logger = Logger.getLogger("Minecraft");
+
+}
