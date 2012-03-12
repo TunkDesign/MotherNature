3ff24c810cf85300a1afc5997f9ba0b1464fcaad
TunkDesign <rsmusicrs@hotmail.com>
2012-03-12 22:55:08 +0100

    Cleaned.

--- src/com/tunkdesign/mothernature/commands/LwandCommand.java
+++ src/com/tunkdesign/mothernature/commands/LwandCommand.java
@@ -1,11 +1,12 @@
 package com.TunkDesign.MotherNature.commands;
 
+//Local import
 import com.TunkDesign.MotherNature.*;
+//Bukkit import
 import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-//import org.bukkit.inventory.PlayerInventory;
 
 public class LwandCommand
     implements CommandExecutor
@@ -19,16 +20,16 @@ public class LwandCommand
     {
         Player player = (Player)sender;
         if(!player.equals(null) && MotherNaturePermissions.has(player, "mothernature.command.lwand"))
-            if(!player.getInventory().contains(MotherNatureSettings.lightningWand))
+            if(!player.getInventory().contains(MotherNature.lightningWand))
             {
-                ItemStack item = new ItemStack(MotherNatureSettings.lightningWand);
+                ItemStack item = new ItemStack(MotherNature.lightningWand);
                 item.setAmount(1);
                 player.getInventory().addItem(new ItemStack[] {
                     item
                 });
                 player.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append("You have been given a").append(ChatColor.GOLD).append(" lightning wand").toString());
             } else
-            if(player.getInventory().contains(MotherNatureSettings.lightningWand))
+            if(player.getInventory().contains(MotherNature.lightningWand))
                 player.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You already have a lightning wand").toString());
             else
                 player.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You don't have the permission for a lightning wand").toString());
