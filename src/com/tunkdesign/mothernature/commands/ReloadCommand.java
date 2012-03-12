99189541e2f6ce5c1d112599b3e417cfa388c570
TunkDesign <rsmusicrs@hotmail.com>
2012-03-12 22:59:40 +0100

    Cleaned and Reloaded console print.

--- src/com/tunkdesign/mothernature/commands/ReloadCommand.java
+++ src/com/tunkdesign/mothernature/commands/ReloadCommand.java
@@ -1,6 +1,8 @@
 package com.TunkDesign.MotherNature.commands;
 
+//Local import
 import com.TunkDesign.MotherNature.*;
+//Bukkit import
 import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
@@ -20,6 +22,7 @@ public class ReloadCommand
         {
             MotherNatureSettings.load();
             player.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append("Mother Nature Settings have been ").append(ChatColor.GOLD).append("Reloaded").toString());
+            MotherNature.log.info("[MotherNature] Settings has been reloaded!");
         } else
         {
             player.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You do not have permission to use this command.").toString());
