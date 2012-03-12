7935d2c80237bf019f3e0698c8abbf9c919df994
Karl Hakansson <webmaster@tunkdesign.com>
2012-03-12 20:17:50 +0100

    File added

---
+++ src/com/tunkdesign/mothernature/commands/RainCommand.java
@@ -0,0 +1,35 @@
+package com.TunkDesign.MotherNature.commands;
+
+import com.TunkDesign.MotherNature.*;
+import com.TunkDesign.MotherNature.thread.MotherNatureThread;
+import org.bukkit.ChatColor;
+//import org.bukkit.World;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+public class RainCommand
+    implements CommandExecutor
+{
+
+    public RainCommand(MotherNature parent)
+    {
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String args[])
+    {
+        Player player = (Player)sender;
+        if(MotherNaturePermissions.has(player, "mothernature.command.rain"))
+        {
+            if(args.length <= 1)
+            {
+                player.getWorld().setStorm(true);
+                MotherNatureThread.rainSteps = MotherNatureSettings.rainInterval;
+                player.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append("You have changed the weather to").append(ChatColor.GOLD).append(" Rain").toString());
+            }
+        } else
+        {
+            player.sendMessage((new StringBuilder()).append(ChatColor.RED).append("You do not have permission to use this command.").toString());
+        }
+        return true;
+    }
+}
